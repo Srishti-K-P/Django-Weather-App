2112270c0dc2fd86e826678d46fefc6052cff676
Srishti K P <115061614+Srishti-K-P@users.noreply.github.com>
2024-07-10 22:03:04 +0530

    Update urls.py

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 
-urlpatterns=[
-    path('', views.index),
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('help/', views.help, name='help'),
+    path('support/', views.support, name='support'),
 ]
